bd390a4918df491e84b23674127debd1a7c4494e
Victoria Landron <viclandron@gmail.com>
2021-01-10 19:10:41 -0500

    adding resume

--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -4,6 +4,13 @@ function Resume() {
     return(
         <div>
             <h2>Click Here to Download My Resume</h2>
+            <Link to="/files/resume.pdf" target="_blank" download>Download</Link>
+            <h3>My Proficiencies</h3>
+                <ul>
+                    <li>
+
+                    </li>
+                </ul>
         </div>
     )
 }
